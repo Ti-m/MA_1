1284c7589e3197b4ee955fd74a6af84bd4c80367
Ti-m <xaru@gmx.net>
2015-12-17 10:42:25 +0100

    Inserted programtic Data Type selection for the constant blocks in the sysgen modulation block

--- Basic_DMT/initScript.m
+++ Basic_DMT/initScript.m
@@ -26,15 +26,6 @@ assignin('base', 'rpv', precision);
 %'outUDDataType'=precisionModu
 if radio_precision == 1
     
-    set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant1','OutDataTypeStr',precision)
-    set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant2','OutDataTypeStr',precision)
-    set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant3','OutDataTypeStr',precision)
-    set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant4','OutDataTypeStr',precision)
-    set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant5','OutDataTypeStr',precision)
-    set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant6','OutDataTypeStr',precision)
-    set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant7','OutDataTypeStr',precision)
-    set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant8','OutDataTypeStr',precision)
-    
     set_param('Basic_DMT/Modulation/Bit_Mapping_P1/256_QAM/Rect_QAM_Mod','outDtype',precision)
     set_param('Basic_DMT/Modulation/Bit_Mapping_P1/16_QAM/Rect QAM','outDtype',precision)
     set_param('Basic_DMT/Modulation/Bit_Mapping_P2/256_QAM/Rectangular_QAM_Modu','outDtype',precision)
@@ -46,7 +37,28 @@ else
     set_param('Basic_DMT/Modulation/Bit_Mapping_P2/16_QAM/Rectangular_QAM_Modu','outDtype','User-defined','outFracLenMode','User-defined','outFracLen',fracLen,'outUDDataType',precisionModu)
 
 end
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant1','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant2','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant3','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant4','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant5','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant6','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant7','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM/Constant8','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
 
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM1/Constant1','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM1/Constant2','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM1/Constant3','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM1/Constant4','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM1/Constant5','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM1/Constant6','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM1/Constant7','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+set_param('Basic_DMT/Sysgen_Modulation/16-QAM1/Constant8','arith_type','Signed (2''s comp)','bin_pt',fracLen,'n_bits',bitCount)
+%get_param(gcb,'arith_type')
+%get_param(gcb,'bin_pt')
+%get_param(gcb,'n_bits')
+%Outputs all block paths
+%BlockPaths = find_system('Basic_DMT','Type','Block')
 
 %set_param('Basic_DMT/Modulation/Bit-Mapping-P1/256-QAM/Rectangular-QAM-Modulator-Baseband1','OutDataTypeStr',precision)
 %set_param(Basic_DMT,'SimulationCommand','Update')
