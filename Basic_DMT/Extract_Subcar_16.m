e60ee0429e28f99415d3aa9b6b12ae0072d8f020
Ti-m <xaru@gmx.net>
2016-03-30 15:49:16 +0200

    Truncated model to 16Bit. Impl. pipelining for demodulator

--- Basic_DMT/Extract_Subcar_16.m
+++ Basic_DMT/Extract_Subcar_16.m
@@ -2,44 +2,44 @@ function [y1_re,  y1_im,  y2_re,  y2_im,  y3_re,  y3_im,  y4_re, y4_im,...
           y5_re,  y5_im,  y6_re,  y6_im,  y7_re,  y7_im,  y8_re, y8_im,...
           y9_re,  y9_im,  y10_re, y10_im, y11_re, y11_im, y12_re, y12_im,...
           y13_re, y13_im, y14_re, y14_im, y15_re, y15_im, y16_re, y16_im...
-          ]= Extract_Subcar_16(x_re,x_im,M,fracLen,bitCount)
+          ]= Extract_Subcar_16(x_re,x_im,fracLen,bitCountAftFFTRec)
 %  M=4;
 %  fracLen=13;
 %  bitCount=16;
 %Some ideas from UG958 
-x1_re  = xl_slice(x_re, (bitCount+(M+1)*2)*16-1, (bitCount+(M+1)*2)*15); 
-x2_re  = xl_slice(x_re, (bitCount+(M+1)*2)*15-1, (bitCount+(M+1)*2)*14); 
-x3_re  = xl_slice(x_re, (bitCount+(M+1)*2)*14-1, (bitCount+(M+1)*2)*13);  
-x4_re  = xl_slice(x_re, (bitCount+(M+1)*2)*13-1, (bitCount+(M+1)*2)*12); 
-x5_re  = xl_slice(x_re, (bitCount+(M+1)*2)*12-1, (bitCount+(M+1)*2)*11);  
-x6_re  = xl_slice(x_re, (bitCount+(M+1)*2)*11-1, (bitCount+(M+1)*2)*10); 
-x7_re  = xl_slice(x_re, (bitCount+(M+1)*2)*10-1, (bitCount+(M+1)*2)*9);  
-x8_re  = xl_slice(x_re, (bitCount+(M+1)*2)*9-1 , (bitCount+(M+1)*2)*8); 
-x9_re  = xl_slice(x_re, (bitCount+(M+1)*2)*8-1 , (bitCount+(M+1)*2)*7);  
-x10_re = xl_slice(x_re, (bitCount+(M+1)*2)*7-1 , (bitCount+(M+1)*2)*6); 
-x11_re = xl_slice(x_re, (bitCount+(M+1)*2)*6-1 , (bitCount+(M+1)*2)*5);  
-x12_re = xl_slice(x_re, (bitCount+(M+1)*2)*5-1 , (bitCount+(M+1)*2)*4); 
-x13_re = xl_slice(x_re, (bitCount+(M+1)*2)*4-1 , (bitCount+(M+1)*2)*3);  
-x14_re = xl_slice(x_re, (bitCount+(M+1)*2)*3-1 , (bitCount+(M+1)*2)*2);
-x15_re = xl_slice(x_re, (bitCount+(M+1)*2)*2-1 , (bitCount+(M+1)*2)*1);  
-x16_re = xl_slice(x_re, (bitCount+(M+1)*2)-1   , 0  );
+x1_re  = xl_slice(x_re, bitCountAftFFTRec*16-1, bitCountAftFFTRec*15); 
+x2_re  = xl_slice(x_re, bitCountAftFFTRec*15-1, bitCountAftFFTRec*14); 
+x3_re  = xl_slice(x_re, bitCountAftFFTRec*14-1, bitCountAftFFTRec*13);  
+x4_re  = xl_slice(x_re, bitCountAftFFTRec*13-1, bitCountAftFFTRec*12); 
+x5_re  = xl_slice(x_re, bitCountAftFFTRec*12-1, bitCountAftFFTRec*11);  
+x6_re  = xl_slice(x_re, bitCountAftFFTRec*11-1, bitCountAftFFTRec*10); 
+x7_re  = xl_slice(x_re, bitCountAftFFTRec*10-1, bitCountAftFFTRec*9);  
+x8_re  = xl_slice(x_re, bitCountAftFFTRec*9-1 , bitCountAftFFTRec*8); 
+x9_re  = xl_slice(x_re, bitCountAftFFTRec*8-1 , bitCountAftFFTRec*7);  
+x10_re = xl_slice(x_re, bitCountAftFFTRec*7-1 , bitCountAftFFTRec*6); 
+x11_re = xl_slice(x_re, bitCountAftFFTRec*6-1 , bitCountAftFFTRec*5);  
+x12_re = xl_slice(x_re, bitCountAftFFTRec*5-1 , bitCountAftFFTRec*4); 
+x13_re = xl_slice(x_re, bitCountAftFFTRec*4-1 , bitCountAftFFTRec*3);  
+x14_re = xl_slice(x_re, bitCountAftFFTRec*3-1 , bitCountAftFFTRec*2);
+x15_re = xl_slice(x_re, bitCountAftFFTRec*2-1 , bitCountAftFFTRec*1);  
+x16_re = xl_slice(x_re, bitCountAftFFTRec-1   , 0  );
 
-x1_im  = xl_slice(x_im, (bitCount+(M+1)*2)*16-1, (bitCount+(M+1)*2)*15); 
-x2_im  = xl_slice(x_im, (bitCount+(M+1)*2)*15-1, (bitCount+(M+1)*2)*14); 
-x3_im  = xl_slice(x_im, (bitCount+(M+1)*2)*14-1, (bitCount+(M+1)*2)*13);  
-x4_im  = xl_slice(x_im, (bitCount+(M+1)*2)*13-1, (bitCount+(M+1)*2)*12); 
-x5_im  = xl_slice(x_im, (bitCount+(M+1)*2)*12-1, (bitCount+(M+1)*2)*11);  
-x6_im  = xl_slice(x_im, (bitCount+(M+1)*2)*11-1, (bitCount+(M+1)*2)*10); 
-x7_im  = xl_slice(x_im, (bitCount+(M+1)*2)*10-1, (bitCount+(M+1)*2)*9);  
-x8_im  = xl_slice(x_im, (bitCount+(M+1)*2)*9-1 , (bitCount+(M+1)*2)*8); 
-x9_im  = xl_slice(x_im, (bitCount+(M+1)*2)*8-1 , (bitCount+(M+1)*2)*7);  
-x10_im = xl_slice(x_im, (bitCount+(M+1)*2)*7-1 , (bitCount+(M+1)*2)*6); 
-x11_im = xl_slice(x_im, (bitCount+(M+1)*2)*6-1 , (bitCount+(M+1)*2)*5);  
-x12_im = xl_slice(x_im, (bitCount+(M+1)*2)*5-1 , (bitCount+(M+1)*2)*4); 
-x13_im = xl_slice(x_im, (bitCount+(M+1)*2)*4-1 , (bitCount+(M+1)*2)*3);  
-x14_im = xl_slice(x_im, (bitCount+(M+1)*2)*3-1 , (bitCount+(M+1)*2)*2);
-x15_im = xl_slice(x_im, (bitCount+(M+1)*2)*2-1 , (bitCount+(M+1)*2)*1);  
-x16_im = xl_slice(x_im, (bitCount+(M+1)*2-1)   , 0  );
+x1_im  = xl_slice(x_im, bitCountAftFFTRec*16-1, bitCountAftFFTRec*15); 
+x2_im  = xl_slice(x_im, bitCountAftFFTRec*15-1, bitCountAftFFTRec*14); 
+x3_im  = xl_slice(x_im, bitCountAftFFTRec*14-1, bitCountAftFFTRec*13);  
+x4_im  = xl_slice(x_im, bitCountAftFFTRec*13-1, bitCountAftFFTRec*12); 
+x5_im  = xl_slice(x_im, bitCountAftFFTRec*12-1, bitCountAftFFTRec*11);  
+x6_im  = xl_slice(x_im, bitCountAftFFTRec*11-1, bitCountAftFFTRec*10); 
+x7_im  = xl_slice(x_im, bitCountAftFFTRec*10-1, bitCountAftFFTRec*9);  
+x8_im  = xl_slice(x_im, bitCountAftFFTRec*9-1 , bitCountAftFFTRec*8); 
+x9_im  = xl_slice(x_im, bitCountAftFFTRec*8-1 , bitCountAftFFTRec*7);  
+x10_im = xl_slice(x_im, bitCountAftFFTRec*7-1 , bitCountAftFFTRec*6); 
+x11_im = xl_slice(x_im, bitCountAftFFTRec*6-1 , bitCountAftFFTRec*5);  
+x12_im = xl_slice(x_im, bitCountAftFFTRec*5-1 , bitCountAftFFTRec*4); 
+x13_im = xl_slice(x_im, bitCountAftFFTRec*4-1 , bitCountAftFFTRec*3);  
+x14_im = xl_slice(x_im, bitCountAftFFTRec*3-1 , bitCountAftFFTRec*2);
+x15_im = xl_slice(x_im, bitCountAftFFTRec*2-1 , bitCountAftFFTRec*1);  
+x16_im = xl_slice(x_im, bitCountAftFFTRec-1   , 0  );
 
 y1_re = xl_force(x1_re,xlSigned,fracLen);
 y2_re = xl_force(x2_re,xlSigned,fracLen);
--- Basic_DMT/Extract_Subcar_16_Equal.m
+++ Basic_DMT/Extract_Subcar_16_Equal.m
@@ -3,7 +3,7 @@ function [y1_re,  y1_im,  y2_re,  y2_im,  y3_re,  y3_im,  y4_re, y4_im,...
           y9_re,  y9_im,  y10_re, y10_im, y11_re, y11_im, y12_re, y12_im,...
           y13_re, y13_im, y14_re, y14_im, y15_re, y15_im, y16_re, y16_im...
           ]= Extract_Subcar_16_Equal(...
-          x_re,x_im,M,fracLen,bitCount,...
+          x_re,x_im,bitCountAftFFTRec,fracLen,...
           e1_re, e1_im, e2_re, e2_im, e3_re, e3_im, e4_re, e4_im,...
           e5_re, e5_im, e6_re, e6_im, e7_re, e7_im, e8_re, e8_im,...
           e9_re, e9_im, e10_re,e10_im,e11_re,e11_im,e12_re,e12_im,...
@@ -12,39 +12,39 @@ function [y1_re,  y1_im,  y2_re,  y2_im,  y3_re,  y3_im,  y4_re, y4_im,...
 %  fracLen=13;
 %  bitCount=16;
 %Some ideas from UG958 
-x1_re  = xl_slice(x_re, (bitCount+(M+1)*2)*16-1, (bitCount+(M+1)*2)*15); 
-x2_re  = xl_slice(x_re, (bitCount+(M+1)*2)*15-1, (bitCount+(M+1)*2)*14); 
-x3_re  = xl_slice(x_re, (bitCount+(M+1)*2)*14-1, (bitCount+(M+1)*2)*13);  
-x4_re  = xl_slice(x_re, (bitCount+(M+1)*2)*13-1, (bitCount+(M+1)*2)*12); 
-x5_re  = xl_slice(x_re, (bitCount+(M+1)*2)*12-1, (bitCount+(M+1)*2)*11);  
-x6_re  = xl_slice(x_re, (bitCount+(M+1)*2)*11-1, (bitCount+(M+1)*2)*10); 
-x7_re  = xl_slice(x_re, (bitCount+(M+1)*2)*10-1, (bitCount+(M+1)*2)*9);  
-x8_re  = xl_slice(x_re, (bitCount+(M+1)*2)*9-1 , (bitCount+(M+1)*2)*8); 
-x9_re  = xl_slice(x_re, (bitCount+(M+1)*2)*8-1 , (bitCount+(M+1)*2)*7);  
-x10_re = xl_slice(x_re, (bitCount+(M+1)*2)*7-1 , (bitCount+(M+1)*2)*6); 
-x11_re = xl_slice(x_re, (bitCount+(M+1)*2)*6-1 , (bitCount+(M+1)*2)*5);  
-x12_re = xl_slice(x_re, (bitCount+(M+1)*2)*5-1 , (bitCount+(M+1)*2)*4); 
-x13_re = xl_slice(x_re, (bitCount+(M+1)*2)*4-1 , (bitCount+(M+1)*2)*3);  
-x14_re = xl_slice(x_re, (bitCount+(M+1)*2)*3-1 , (bitCount+(M+1)*2)*2);
-x15_re = xl_slice(x_re, (bitCount+(M+1)*2)*2-1 , (bitCount+(M+1)*2)*1);  
-x16_re = xl_slice(x_re, (bitCount+(M+1)*2)-1   , 0  );
+x1_re  = xl_slice(x_re, bitCountAftFFTRec*16-1, bitCountAftFFTRec*15); 
+x2_re  = xl_slice(x_re, bitCountAftFFTRec*15-1, bitCountAftFFTRec*14); 
+x3_re  = xl_slice(x_re, bitCountAftFFTRec*14-1, bitCountAftFFTRec*13);  
+x4_re  = xl_slice(x_re, bitCountAftFFTRec*13-1, bitCountAftFFTRec*12); 
+x5_re  = xl_slice(x_re, bitCountAftFFTRec*12-1, bitCountAftFFTRec*11);  
+x6_re  = xl_slice(x_re, bitCountAftFFTRec*11-1, bitCountAftFFTRec*10); 
+x7_re  = xl_slice(x_re, bitCountAftFFTRec*10-1, bitCountAftFFTRec*9);  
+x8_re  = xl_slice(x_re, bitCountAftFFTRec*9-1 , bitCountAftFFTRec*8); 
+x9_re  = xl_slice(x_re, bitCountAftFFTRec*8-1 , bitCountAftFFTRec*7);  
+x10_re = xl_slice(x_re, bitCountAftFFTRec*7-1 , bitCountAftFFTRec*6); 
+x11_re = xl_slice(x_re, bitCountAftFFTRec*6-1 , bitCountAftFFTRec*5);  
+x12_re = xl_slice(x_re, bitCountAftFFTRec*5-1 , bitCountAftFFTRec*4); 
+x13_re = xl_slice(x_re, bitCountAftFFTRec*4-1 , bitCountAftFFTRec*3);  
+x14_re = xl_slice(x_re, bitCountAftFFTRec*3-1 , bitCountAftFFTRec*2);
+x15_re = xl_slice(x_re, bitCountAftFFTRec*2-1 , bitCountAftFFTRec*1);  
+x16_re = xl_slice(x_re, bitCountAftFFTRec-1   , 0  );
 
-x1_im  = xl_slice(x_im, (bitCount+(M+1)*2)*16-1, (bitCount+(M+1)*2)*15); 
-x2_im  = xl_slice(x_im, (bitCount+(M+1)*2)*15-1, (bitCount+(M+1)*2)*14); 
-x3_im  = xl_slice(x_im, (bitCount+(M+1)*2)*14-1, (bitCount+(M+1)*2)*13);  
-x4_im  = xl_slice(x_im, (bitCount+(M+1)*2)*13-1, (bitCount+(M+1)*2)*12); 
-x5_im  = xl_slice(x_im, (bitCount+(M+1)*2)*12-1, (bitCount+(M+1)*2)*11);  
-x6_im  = xl_slice(x_im, (bitCount+(M+1)*2)*11-1, (bitCount+(M+1)*2)*10); 
-x7_im  = xl_slice(x_im, (bitCount+(M+1)*2)*10-1, (bitCount+(M+1)*2)*9);  
-x8_im  = xl_slice(x_im, (bitCount+(M+1)*2)*9-1 , (bitCount+(M+1)*2)*8); 
-x9_im  = xl_slice(x_im, (bitCount+(M+1)*2)*8-1 , (bitCount+(M+1)*2)*7);  
-x10_im = xl_slice(x_im, (bitCount+(M+1)*2)*7-1 , (bitCount+(M+1)*2)*6); 
-x11_im = xl_slice(x_im, (bitCount+(M+1)*2)*6-1 , (bitCount+(M+1)*2)*5);  
-x12_im = xl_slice(x_im, (bitCount+(M+1)*2)*5-1 , (bitCount+(M+1)*2)*4); 
-x13_im = xl_slice(x_im, (bitCount+(M+1)*2)*4-1 , (bitCount+(M+1)*2)*3);  
-x14_im = xl_slice(x_im, (bitCount+(M+1)*2)*3-1 , (bitCount+(M+1)*2)*2);
-x15_im = xl_slice(x_im, (bitCount+(M+1)*2)*2-1 , (bitCount+(M+1)*2)*1);  
-x16_im = xl_slice(x_im, (bitCount+(M+1)*2-1)   , 0  );
+x1_im  = xl_slice(x_im, bitCountAftFFTRec*16-1, bitCountAftFFTRec*15); 
+x2_im  = xl_slice(x_im, bitCountAftFFTRec*15-1, bitCountAftFFTRec*14); 
+x3_im  = xl_slice(x_im, bitCountAftFFTRec*14-1, bitCountAftFFTRec*13);  
+x4_im  = xl_slice(x_im, bitCountAftFFTRec*13-1, bitCountAftFFTRec*12); 
+x5_im  = xl_slice(x_im, bitCountAftFFTRec*12-1, bitCountAftFFTRec*11);  
+x6_im  = xl_slice(x_im, bitCountAftFFTRec*11-1, bitCountAftFFTRec*10); 
+x7_im  = xl_slice(x_im, bitCountAftFFTRec*10-1, bitCountAftFFTRec*9);  
+x8_im  = xl_slice(x_im, bitCountAftFFTRec*9-1 , bitCountAftFFTRec*8); 
+x9_im  = xl_slice(x_im, bitCountAftFFTRec*8-1 , bitCountAftFFTRec*7);  
+x10_im = xl_slice(x_im, bitCountAftFFTRec*7-1 , bitCountAftFFTRec*6); 
+x11_im = xl_slice(x_im, bitCountAftFFTRec*6-1 , bitCountAftFFTRec*5);  
+x12_im = xl_slice(x_im, bitCountAftFFTRec*5-1 , bitCountAftFFTRec*4); 
+x13_im = xl_slice(x_im, bitCountAftFFTRec*4-1 , bitCountAftFFTRec*3);  
+x14_im = xl_slice(x_im, bitCountAftFFTRec*3-1 , bitCountAftFFTRec*2);
+x15_im = xl_slice(x_im, bitCountAftFFTRec*2-1 , bitCountAftFFTRec*1);  
+x16_im = xl_slice(x_im, bitCountAftFFTRec-1   , 0  );
 
 tmp_y1_re = xl_force(x1_re,xlSigned,fracLen);
 tmp_y2_re = xl_force(x2_re,xlSigned,fracLen);
@@ -85,39 +85,39 @@ tmp_y16_im = xl_force(x16_im,xlSigned,fracLen);
 %prepared in matlab(1/e))
 
 %convert coefficients from double to fixpoint
-e1_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e1_re);
-e2_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e2_re);
-e3_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e3_re);
-e4_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e4_re);
-e5_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e5_re);
-e6_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e6_re);
-e7_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e7_re);
-e8_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e8_re);
-e9_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e9_re);
-e10_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e10_re);
-e11_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e11_re);
-e12_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e12_re);
-e13_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e13_re);
-e14_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e14_re);
-e15_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e15_re);
-e16_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e16_re);
+e1_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e1_re);
+e2_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e2_re);
+e3_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e3_re);
+e4_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e4_re);
+e5_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e5_re);
+e6_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e6_re);
+e7_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e7_re);
+e8_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e8_re);
+e9_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e9_re);
+e10_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e10_re);
+e11_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e11_re);
+e12_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e12_re);
+e13_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e13_re);
+e14_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e14_re);
+e15_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e15_re);
+e16_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e16_re);
 
-e1_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e1_im);
-e2_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e2_im);
-e3_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e3_im);
-e4_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e4_im);
-e5_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e5_im);
-e6_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e6_im);
-e7_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e7_im);
-e8_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e8_im);
-e9_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e9_im);
-e10_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e10_im);
-e11_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e11_im);
-e12_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e12_im);
-e13_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e13_im);
-e14_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e14_im);
-e15_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e15_im);
-e16_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, e16_im);
+e1_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e1_im);
+e2_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e2_im);
+e3_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e3_im);
+e4_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e4_im);
+e5_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e5_im);
+e6_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e6_im);
+e7_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e7_im);
+e8_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e8_im);
+e9_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e9_im);
+e10_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e10_im);
+e11_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e11_im);
+e12_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e12_im);
+e13_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e13_im);
+e14_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e14_im);
+e15_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, e15_im);
+e16_im = xfix({xlSigned,bitCountAftFFTRec,fracLen}, e16_im);
 
 %apply coefficients (complex multiplication)
 y1_re = tmp_y1_re*e9_re- tmp_y1_im*e9_im;
@@ -188,38 +188,38 @@ y16_im = tmp_y16_re*e8_im+ tmp_y16_im*e8_re;
 % y15_im = xl_force(y15_im,xlSigned,fracLen);
 % y16_im = xl_force(y16_im,xlSigned,fracLen);
 
-y1_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y1_re);
-y2_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y2_re);
-y3_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y3_re);
-y4_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y4_re);
-y5_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y5_re);
-y6_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y6_re);
-y7_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y7_re);
-y8_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y8_re);
-y9_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y9_re);
-y10_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y10_re);
-y11_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y11_re);
-y12_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y12_re);
-y13_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y13_re);
-y14_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y14_re);
-y15_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y15_re);
-y16_re = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y16_re);
+y1_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y1_re);
+y2_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y2_re);
+y3_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y3_re);
+y4_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y4_re);
+y5_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y5_re);
+y6_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y6_re);
+y7_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y7_re);
+y8_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y8_re);
+y9_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y9_re);
+y10_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y10_re);
+y11_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y11_re);
+y12_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y12_re);
+y13_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y13_re);
+y14_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y14_re);
+y15_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y15_re);
+y16_re = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y16_re);
 
-y1_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y1_im);
-y2_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y2_im);
-y3_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y3_im);
-y4_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y4_im);
-y5_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y5_im);
-y6_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y6_im);
-y7_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y7_im);
-y8_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y8_im);
-y9_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y9_im);
-y10_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y10_im);
-y11_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y11_im);
-y12_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y12_im);
-y13_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y13_im);
-y14_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y14_im);
-y15_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y15_im);
-y16_im = xfix({xlSigned, bitCount+(M+1)*2,fracLen}, y16_im);
+y1_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y1_im);
+y2_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y2_im);
+y3_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y3_im);
+y4_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y4_im);
+y5_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y5_im);
+y6_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y6_im);
+y7_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y7_im);
+y8_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y8_im);
+y9_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y9_im);
+y10_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y10_im);
+y11_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y11_im);
+y12_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y12_im);
+y13_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y13_im);
+y14_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y14_im);
+y15_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y15_im);
+y16_im = xfix({xlSigned, bitCountAftFFTRec,fracLen}, y16_im);
 
   end
