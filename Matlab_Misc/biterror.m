b1ed28321cc1a3cc6b014a13cce2d1707d593003
Ti-m <xaru@gmx.net>
2016-05-10 17:17:12 +0200

    testing SNR of AWGN

--- Matlab_Misc/biterror.m
+++ Matlab_Misc/biterror.m
@@ -2,8 +2,13 @@
 close all, clear
 Fs=16/896e-9;
 Rb=62.5e6;
-SNRdb = 0:0.1:20-0.1;
+SNRdb = 0:0.1:30-0.1;
 SNR=10.^(SNRdb/10);
 
 pb=3/2*erfc(sqrt(SNR*2*Fs/5/Rb));
-semilogy(SNRdb,pb)
+semilogy(SNRdb,pb),grid
+
+%OFDM,SNR18,no_chan 
+%theo 2e-4
+
+%OFDM,SNR18,chan/equ
